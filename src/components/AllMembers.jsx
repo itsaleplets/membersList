5c58fd9fbd03e5d2c051d1a9f8130e1dd265da29
Alessandra <itsalematos@hotmail.com>
2021-03-26 16:52:37 -0400

    Feat: filter employee by login

--- src/components/AllMembers.jsx
+++ src/components/AllMembers.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 
 function AllMembers() {
   const [members, setMembers] = useState('');
+  const [loginFilter, setLoginFilter] = useState('');
+
   useEffect(() => {
     getData();
   }, []);
@@ -10,17 +12,26 @@ function AllMembers() {
   const getData = async () => {
     const data = await getAllMembers();
     setMembers(data);
-  }
+  };
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    console.log(value);
+    setLoginFilter(value);
+  };
 
   return (
     <div>
-      {members && members.map((member) => {
-        return <div>
+      <input
+        placeholder="Login"
+        onChange={handleChange}
+      />
+      {members && members.filter((member) => member.login.toLowerCase().includes(loginFilter.toLowerCase())).map((member) => (
+        <div>
           <h3>{`Login: ${member.login}`}</h3>
           <img src={member.avatar_url} alt={`${member.login} github avatar`}/>
-                
-          </div>
-      })}
+        </div>
+      ))}
     </div>
   );
 }
